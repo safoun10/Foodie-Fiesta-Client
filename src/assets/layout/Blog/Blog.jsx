e913e06713a3e021c7d66cc5d3500cbef2dd73da
Safoun <safoun.official@gmail.com>
2023-05-04 18:09:22 +0600

    made blog site

--- src/assets/layout/Blog/Blog.jsx
+++ src/assets/layout/Blog/Blog.jsx
@@ -1,14 +1,186 @@
-import React from 'react';
-import TopNav from '../../components/topNav/TopNav';
-import Footer from '../../components/footer/Footer';
-import "./Blog.css"
+import React from "react";
+import TopNav from "../../components/topNav/TopNav";
+import Footer from "../../components/footer/Footer";
+import "./Blog.css";
 
 const Blog = () => {
-    return (
+	return (
 		<div className="pb-1">
 			<TopNav></TopNav>
-			<section className='mx-auto' style={{maxWidth : "1200px"}}>
-				<div></div>
+			<section
+				className="mx-auto my-5 py-5"
+				style={{ maxWidth: "1200px" }}
+			>
+				<div>
+					<div className="display-5 fw-bold mb-5 pb-4">
+						What is the Difference Between a Controlled and
+						Uncontrolled Component in React?
+					</div>
+					<div className="title-text fs-5">
+						In React, there are two types of components:{" "}
+						<b>controlled</b> and <b>uncontrolled</b>. Uncontrolled
+						components refer to those where the browser or an
+						external component manages the state. For instance, a
+						basic HTML input element is an uncontrolled component in
+						React because it takes care of its own state. These
+						components are used for simple inputs where the
+						application doesn't need to keep track of the state.
+						<br />
+						<br />
+						Controlled components, on the other hand, are components
+						where the application manages the state using the state
+						or props objects. For example, a React input component
+						that updates its value based on the application's state
+						is a controlled component. These components offer more
+						precise control over how the component behaves and
+						looks. Controlled components are usually used for more
+						complex user interactions.
+						<br />
+						<br />
+						Controlled components manage state through props, making
+						them predictable but complex in large projects.
+						Uncontrolled components manage their own state with
+						refs, making them simpler but harder to track.
+						Uncontrolled components are also difficult to test
+						because internal state is not visible.
+					</div>
+				</div>
+				<div className="mt-5 pt-5">
+					<div className="display-5 fw-bold mb-5 pb-4">
+						How to validate React props using PropTypes ?
+					</div>
+					<div className="title-text fs-5">
+						In web development, components refer to the building
+						blocks of user interfaces. They can be either controlled
+						or uncontrolled. Controlled components are those whose
+						values are controlled by the parent component, while
+						uncontrolled components manage their own state.
+						Understanding the differences between the two is
+						important for building efficient and maintainable web
+						applications.
+						<br />
+						<br />
+						Uncontrolled components are simpler to use and require
+						less code, as they manage their own state internally.
+						They are commonly used for simple forms that do not
+						require validation or complex input handling. However,
+						uncontrolled components can cause unexpected behavior
+						and are harder to test. Also, they don't support
+						server-side rendering and can cause performance issues
+						when used in larger applications.
+						<br />
+						<br />
+						On the other hand, controlled components give developers
+						more control over the behavior of the application. They
+						are particularly useful for complex forms that require
+						input validation or dynamic input handling. Controlled
+						components can be tested more easily and support
+						server-side rendering. However, they require more code
+						and can be more difficult to implement.
+						<br />
+						<br />
+						In conclusion, choosing between controlled and
+						uncontrolled components depends on the specific needs of
+						the application. For simple forms, uncontrolled
+						components may be sufficient, while for more complex
+						forms and larger applications, controlled components are
+						usually preferred. Developers should weigh the benefits
+						and drawbacks of each approach to determine the best fit
+						for their particular use case.
+					</div>
+				</div>
+				<div className="mt-5 pt-5">
+					<div className="display-5 fw-bold mb-5 pb-4">
+						What is the difference between Node.js and Express.js ?
+					</div>
+					<div className="title-text fs-5">
+						Node.js and Express.js are two popular technologies used
+						in web development. While Node.js is a JavaScript
+						runtime environment that allows developers to run
+						JavaScript code on the server-side, Express.js is a web
+						application framework that is built on top of Node.js
+						and provides a set of features for building web
+						applications.
+						<br />
+						<br />
+						Node.js provides a platform for executing JavaScript on
+						the server-side, which enables developers to write
+						server-side code using the same language as the
+						client-side code. Node.js provides features like
+						event-driven, non-blocking I/O that make it efficient
+						for handling high-traffic web applications. Node.js can
+						be used for a variety of server-side tasks, including
+						building APIs, handling file uploads, and processing
+						data.
+						<br />
+						<br />
+						Express.js, on the other hand, is a lightweight web
+						application framework that simplifies the process of
+						building web applications using Node.js. It provides a
+						set of features, such as routing, middleware, and
+						template engines, that make it easier to build scalable
+						and maintainable web applications. Express.js is
+						designed to work with Node.js and leverages its features
+						to provide an efficient and easy-to-use web development
+						platform.
+						<br />
+						<br />
+						While Node.js provides a platform for executing
+						JavaScript on the server-side, Express.js is a web
+						application framework that simplifies the process of
+						building web applications using Node.js. Node.js is
+						suitable for a variety of server-side tasks, while
+						Express.js is designed specifically for building web
+						applications. Both technologies are widely used in web
+						development and offer unique advantages depending on the
+						specific needs of the project.
+					</div>
+				</div>
+				<div className="mt-5 pt-5">
+					<div className="display-5 fw-bold mb-5 pb-4">
+						What is a custom hook, and why will you create a custom
+						hook ?
+					</div>
+					<div className="title-text fs-5">
+						React is a popular JavaScript library used for building
+						user interfaces. Custom hooks are a powerful feature of
+						React that allows developers to reuse logic across
+						multiple components. A custom hook is simply a
+						JavaScript function that uses the built-in hooks
+						provided by React, such as useState or useEffect, to
+						encapsulate and reuse a specific piece of logic.
+						<br />
+						<br />
+						Creating a custom hook can provide several benefits.
+						First, it can help reduce code duplication and make the
+						codebase more maintainable. Custom hooks can be shared
+						across multiple components, allowing developers to
+						centralize the logic and avoid repeating the same code
+						in different places. Second, custom hooks can help
+						improve the readability of the code. By abstracting away
+						complex logic into a custom hook, developers can make
+						the code more modular and easier to understand.
+						<br />
+						<br />
+						Developers can create custom hooks for a wide range of
+						use cases. For example, a custom hook could be created
+						to handle form validation, authentication, or data
+						fetching. By encapsulating this logic in a custom hook,
+						developers can easily reuse the same code across
+						multiple components, reducing the amount of boilerplate
+						code required in each component.
+						<br />
+						<br />
+						Custom hooks are a powerful feature of
+						React that allows developers to encapsulate and reuse
+						logic across multiple components. Creating a custom hook
+						can help reduce code duplication, improve
+						maintainability, and make the codebase more modular and
+						readable. Developers can create custom hooks for a wide
+						range of use cases, making them an essential tool in the
+						React developer's toolkit.
+					</div>
+				</div>
 			</section>
 			<section className="bg-footer">
 				<Footer></Footer>
